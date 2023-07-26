63bb770929255d74834ff67a2d90463883cc99a7
Kiyan <fourcels@gmail.com>
2023-07-26 16:43:09 +0800

    entry

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-    entry: ['plugin-react-pages/index.ts'],
+    entry: ['vite-plugin-react-pages/index.ts'],
     format: 'esm',
     clean: true,
     dts: true,
